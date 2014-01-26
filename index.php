366a7e60d0d766569c122f4abbb60595d38ccc6c
ArmandoPC <armand1live@gmail.com>
2014-01-26 18:08:42 -0500

    Carga de proyecto Metodo Simplex Maximizacion

--- index.php
+++ index.php
@@ -55,8 +55,7 @@
                     </div>
                 </div>
             </div>
-            <script type="text/javascript" src="js/bootstrap.min.js" ></script>
             <script type="text/javascript" src="js/jquery-1.10.2.js" ></script>
-
+            <script type="text/javascript" src="js/bootstrap.min.js" ></script>
         </body>
     </html>
